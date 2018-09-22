b7a033b144f63584043da89159ddca4d8c67b776
Chenduoru <chenduoru21@qq.com>
2018-09-22 13:58:26 +0800

    daychan

--- 后端+识别/server/recognition/reco.controller.js
+++ 后端+识别/server/recognition/reco.controller.js
@@ -11,8 +11,15 @@ const httpStatus = require('http-status');
 const APIError = require('../helpers/APIError');
 const config = require('../../config/config');
 
-function getLocalTime(nS) {     
-  return new Date(parseInt(nS) * 1000).toLocaleString().substr(0,17)
+function getLocalTime(date){
+  var date = new Date(date*1000);//如果date为13位不需要乘1000
+  var Y = date.getFullYear() + '-';
+  var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+  var D = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate()) + ' ';
+  var h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
+  var m = (date.getMinutes() <10 ? '0' + date.getMinutes() : date.getMinutes()) + ':';
+  var s = (date.getSeconds() <10 ? '0' + date.getSeconds() : date.getSeconds());
+  return Y+M+D+h+m+s;
 }
 
 function handlePicsByUser(req, res, next) {
